65d6fbb54db94d402515a0b9d11c4deb3e0ac438
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-04-14 14:22:35 +0300

    добавлены горячие клавиши

--- UI/main.py
+++ UI/main.py
@@ -3,6 +3,7 @@ import os.path
 
 import customtkinter as ctk
 
+from logic import UpdateTheme, CheckData
 from UI.main_windows import MainWindows
 from UI.settings_windows import SettingsWindow
 from complite_processing_file import CompliteProcessingFile
@@ -50,11 +51,32 @@ class Window(ctk.CTk):
         self.grid_columnconfigure(index=0, weight=1)
         self.grid_rowconfigure(index=0, weight=1)
 
+        # настройка цветовой темы
+        self.bind("<Control-d>", lambda a: UpdateTheme.update_theme("dark"))
+        self.bind("<Control-l>", lambda a: UpdateTheme.update_theme("light"))
+        self.bind("<Control-s>", lambda a: UpdateTheme.update_theme("system"))
+
+        # открытие/закрытие настроек
+        self.bind("<Control-q>", lambda a: self.open_menu_keys())
+
+
         self.open_base_menu()
+
+    def open_menu_keys(self):
+        if hasattr(self, 'settings_frame'):
+            self.settings_frame.destroy()
+            del self.settings_frame
+            self.open_base_menu()
+        elif hasattr(self, 'main_frame'):
+            self.main_frame.destroy()
+            del self.main_frame
+            self.open_settings()
+
 
     def open_base_menu(self):
         if hasattr(self, 'settings_frame'):
             self.settings_frame.destroy()
+            del self.settings_frame
         elif hasattr(self, 'frame'):
             try:
                 self.frame.destroy()
@@ -64,9 +86,16 @@ class Window(ctk.CTk):
         self.main_frame = MainWindows(master=self, base=self)
         self.main_frame.grid(row=0, column=0, sticky='nsew')
 
+        self.bind("<Return>", lambda a: CheckData.check_data(self.main_frame))
+        self.bind("<Control-r>", lambda a: self.main_frame.entry_frame_3.click_password())
+        self.bind("<Control-f>", lambda a: self.main_frame.entry_frame_2.click_file())
+        self.bind("<Control-x>", lambda a: self.main_frame.radiobutton_frame.new_state_radio_button())
+        self.bind("<Control-w>", lambda a: self.main_frame.entry_frame_1.click_path())
+
     def open_settings(self):
         if hasattr(self, 'main_frame'):
             self.main_frame.destroy()
+            del self.main_frame
 
         self.settings_frame = SettingsWindow(master=self, base=self)
         self.settings_frame.grid(row=0, column=0, sticky="nsew")
